46e2b5a8a2612e733a6a77183bd11a281edc9510
AntoninSlejska <antonin@slejska.eu>
2017-02-01 20:30:43 +0100

    Applies Drupal guidelines

--- src/Controller/OpenSearchDescriptionController.php
+++ src/Controller/OpenSearchDescriptionController.php
@@ -1,8 +1,4 @@
 <?php
-/**
- * @file
- * Contains \Drupal\opensearchtab\Controller\OpenSearchDescriptionController class.
- */
 
 namespace Drupal\opensearchtab\Controller;
 
@@ -11,33 +7,34 @@ use Symfony\Component\HttpFoundation\Response;
 /**
  * OpenSearchDescriptionController .
  */
-class OpenSearchDescriptionController
-{
-    /**
-     * {@inheritdoc}
-     */
-    public function content() {
-        // Prepare variables for the XML content.
-        $config = \Drupal::config('system.site');
-        $short_name = !empty($config->get('name')) ? $config->get('name') : 'Drupal site';
-        $description = !empty($config->get('slogan')) ? $config->get('slogan') : 'Search the site';
-        $template = \Drupal::request()->getSchemeAndHttpHost() . '/search/node?keys={searchTerms}';
+class OpenSearchDescriptionController {
 
-        // Define the XML content.
-        $openTag = new \SimpleXMLElement('<OpenSearchDescription></OpenSearchDescription>');
-        $openTag->addAttribute('xmlns', 'http://a9.com/-/spec/opensearch/1.1/');
-        $openTag->addChild('ShortName', $short_name);
-        $openTag->addChild('Description', $description);
-        $url = $openTag->addChild('Url');
-        $url->addAttribute('type', 'text/html');
-        $url->addAttribute('method', 'get');
-        $url->addAttribute('template', $template);
+  /**
+   * {@inheritdoc}
+   */
+  public function content() {
+    // Prepare variables for the XML content.
+    $config = \Drupal::config('system.site');
+    $short_name = !empty($config->get('name')) ? $config->get('name') : 'Drupal site';
+    $description = !empty($config->get('slogan')) ? $config->get('slogan') : 'Search the site';
+    $template = \Drupal::request()->getSchemeAndHttpHost() . '/search/node?keys={searchTerms}';
 
-        // Prepare the response.
-        $response = new Response();
-        $response->setContent($openTag->asXML());
-        $response->headers->set('Content-type', 'text/xml');
+    // Define the XML content.
+    $openTag = new \SimpleXMLElement('<OpenSearchDescription></OpenSearchDescription>');
+    $openTag->addAttribute('xmlns', 'http://a9.com/-/spec/opensearch/1.1/');
+    $openTag->addChild('ShortName', $short_name);
+    $openTag->addChild('Description', $description);
+    $url = $openTag->addChild('Url');
+    $url->addAttribute('type', 'text/html');
+    $url->addAttribute('method', 'get');
+    $url->addAttribute('template', $template);
 
-        return $response;
-    }
+    // Prepare the response.
+    $response = new Response();
+    $response->setContent($openTag->asXML());
+    $response->headers->set('Content-type', 'text/xml');
+
+    return $response;
+  }
+
 }
